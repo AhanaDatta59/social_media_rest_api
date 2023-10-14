8fb6fd22b43c0b9806a5b2ecd2e7f761017e7615
Ahana Datta <116972422+AhanaDatta59@users.noreply.github.com>
2023-10-14 12:53:50 +0530

    Users and Blogs Completed

--- backend/controllers/blog-controller.js
+++ backend/controllers/blog-controller.js
@@ -27,3 +27,49 @@ export const getAllBlogs = async (req, res, next) => {
     }
     return res.status(200).json({blog})
   };
+
+  export const updateBlog = async(req,res,next) => {
+    const {title, description}=req.body;
+    const blogId = req.params.id;
+    let blog;
+   try{
+    blog = await Blog.findByIdAndUpdate(blogId,{
+        title,
+        description
+    })
+   }catch(err){
+    return console.log(err);
+   }
+   if(!blog){
+    return res.status(500).json({message: "Unable to Update the Blog"})
+   }
+   return res.status(200).json({blog})
+  }
+
+  export const getById = async (req,res,next) => {
+    const id=req.params.id;
+    let blog;
+    try{
+        blog = await Blog.findById(id);
+    }catch(err){
+        return console.log(err);
+    }
+    if(!blog){
+        return res.status(404).json({message: "No Blog Found"})
+    }
+    return res.status(200).json({blog})
+  }
+
+  export const deleteBlog = async(req,res,next) => {
+    const id=req.params.id;
+    let blog;
+    try{
+        blog=await Blog.findByIdAndRemove(id)
+    }catch(err){
+        return console.log(err)
+    }
+    if(!blog){
+        return res.status(500).json({message:"Unable to Delete"})
+    }
+    return res.status(200).json({message:"Successfully Deleted"})
+  }
--- backend/routes/blog-routes.js
+++ backend/routes/blog-routes.js
@@ -1,10 +1,13 @@
 import express from 'express';
-import { addBlog, getAllBlogs } from '../controllers/blog-controller.js';
+import { addBlog, deleteBlog, getAllBlogs, getById, updateBlog } from '../controllers/blog-controller.js';
 
 const blogRouter = express.Router();//grabbing the router
 
 blogRouter.get("/", getAllBlogs);
 blogRouter.post("/add",addBlog);
+blogRouter.put("/update/:id",updateBlog)//ipdating the user//id from mongoDB
+blogRouter.get("/:id",getById);
+blogRouter.delete("/:id",deleteBlog)
 
 export default blogRouter;
 
